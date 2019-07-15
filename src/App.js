8329312f79ef3a6c9e6630738e17fab0a0e538ed
kiqecanijo <enry1375@gmail.com>
2019-07-15 01:00:40 -0500

    almost listing items

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css'
 import axios from 'axios'
 import styled from 'styled-components'
 
+const ListItems = styled.div`
+  display: table-cell;
+  vertical-align: bottom;
+`
+
 class App extends Component {
   state = {
     baseImg: 'http://image.tmdb.org/t/p/w',
@@ -16,7 +21,9 @@ class App extends Component {
     const { requestURl, apiKey, types } = this.state
     types.map(type =>
       axios
-        .get(`${requestURl}${type}?api_key=${apiKey}&language=en-US&page=1`)
+        .get(
+          `${requestURl}${type}?api_key=${apiKey}&page=15&language=en-US&page=1`
+        )
         .then(res => this.setState({ [type]: res.data.results }))
         .catch(error => console.log('an error ocurred 😞'))
     )
@@ -27,14 +34,22 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {types.map(type => ({ type, list: this.state[type] })).map(el => {
-            return (
-              <div>
-                <h2>{el.type}:</h2>
-                {el.list && el.list.map(movie => <p>{movie.title}</p>)}
-              </div>
-            )
-          })}
+          {types.map(type => ({ type, list: this.state[type] })).map(el => (
+            <div>
+              <h2>{el.type}:</h2>
+              {el.list && console.log(el.list)}
+              {el.list &&
+                el.list.map(movie => (
+                  <ListItems>
+                    <p>{movie.title}</p>
+                    <img
+                      onMouseOver={event => console.log(event.target.src)}
+                      src={`${baseImg}${size}${movie.poster_path}`}
+                    />
+                  </ListItems>
+                ))}
+            </div>
+          ))}
         </header>
       </div>
     )
